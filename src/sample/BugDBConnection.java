099617a9dcb02853e1efdf357b486a7bff50276d
rose-PC\rose <hanul1546@gmail.com>
2017-05-10 21:58:29 +0900

    implement sample experiment

--- src/sample/BugDBConnection.java
+++ src/sample/BugDBConnection.java
@@ -1,12 +1,13 @@
 package sample;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 
 public class BugDBConnection {
 	static public HashMap<String, Connection> connMap = new HashMap<String, Connection>();;
@@ -15,17 +16,54 @@ public class BugDBConnection {
 	BugDBConnection() throws Exception
 	{
 		Class.forName("org.h2.Driver");
-		Connection conn = DriverManager.getConnection("jdbc:h2:./DB/birt/birt","sa","");
-		System.out.println("-------- CONNECT WITH BIRT BIRT DB ----------");;
-		connMap.put("birt-birt", conn);
+		BufferedReader br = new BufferedReader(new FileReader("./data/domain.csv"));
+		String str;
+		while((str=br.readLine())!= null){
+			String[] line = str.split(",");
+			System.out.println(line[0] +" "+line[1].replace("?", ""));
+			String domain = line[0];
+			String project = line[1].replace("?", "");
+
+			Connection conn = DriverManager.getConnection("jdbc:h2:./DB/"+domain+"/"+project,"sa","");
+			System.out.println("-------- CONNECT WITH "+domain+" "+project+" DB ----------");;
+
+			connMap.put(domain.toLowerCase()+"-"+project.toLowerCase(), conn);
+			
+		}
 	}
 	
 	public static void main(String[] args) throws Exception {		
 		new BugDBConnection();
 		
+		Connection cheConnection = connMap.get("science-ice");
+				
 		try
 		{
-			Statement q = connMap.get("birt-birt").createStatement();
+			int bugid= 483617;
+			Statement q = cheConnection.createStatement();
+			ResultSet rs = q.executeQuery("SELECT * FROM history where bug_id = "+bugid+" order by bug_id, date");			
+			while(rs.next()){
+				System.out.println(rs.getInt("BUG_ID")+" = "+rs.getString("DATE")+ " : "+rs.getString("FIELD")+" = "+rs.getString("prev")+" -> "+rs.getString("post"));
+				if(rs.getString("field").toLowerCase().equals("assignee")){
+					Statement q2 = cheConnection.createStatement();
+					ResultSet rs2 = q2.executeQuery("SELECT * FROM meta_field where bug_id = "+bugid);			
+					while(rs2.next()){
+						System.out.println(rs2.getInt("BUG_ID")+" = "+rs2.getString("assignee"));
+						
+					}
+				}
+				
+			}
+			
+		}
+		catch(Exception e1)
+		{		
+			e1.printStackTrace();
+		}
+		
+		try
+		{
+			Statement q = cheConnection.createStatement();
 			ResultSet rs = q.executeQuery("SELECT * FROM history WHERE PARSEDATETIME(date,'yyyy-MM-dd hh:mm:ss') < DATE '2007-02-21'  order by date asc");			
 			while(rs.next()){
 				System.out.println(rs.getInt("BUG_ID")+" = "+rs.getString("DATE")+ " : "+rs.getString("FIELD")+" = "+rs.getString("prev")+" -> "+rs.getString("post"));
@@ -43,7 +81,7 @@ public class BugDBConnection {
 		
 		try
 		{
-			Statement q = connMap.get("birt-birt").createStatement();
+			Statement q = cheConnection.createStatement();
 			ResultSet rs = q.executeQuery("SELECT * FROM history WHERE PARSEDATETIME(date,'yyyy-MM-dd hh:mm:ss') < DATE '"+targetDate+"'  order by date asc");			
 			while(rs.next()){
 				System.out.println(rs.getInt("BUG_ID")+" = "+rs.getString("DATE")+ " : "+rs.getString("FIELD")+" = "+rs.getString("prev")+" -> "+rs.getString("post"));
@@ -54,7 +92,6 @@ public class BugDBConnection {
 		{		
 			e1.printStackTrace();
 		}
-		
 	}
 
 }
